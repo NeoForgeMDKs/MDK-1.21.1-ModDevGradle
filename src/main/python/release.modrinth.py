5106b7c36dde684aa125422b3b7f7db0ad5de01d
Mochi753 <179483962+mochi-753@users.noreply.github.com>
2026-06-15 03:47:26 +0900

    refactor(ci): metadata→data

--- src/main/python/release.modrinth.py
+++ src/main/python/release.modrinth.py
@@ -8,7 +8,7 @@ import requests
 def main():
     MODRINTH_TOKEN = os.environ.get("MODRINTH_TOKEN")
 
-    metadata = {
+    data = {
         "name": f"{os.environ.get("REPOSITORY_NAME")} {os.environ.get("VERSION")}",
         "version_number": os.environ.get("VERSION").removeprefix("v"),
         "changelog": Path("CHANGELOG.md").read_text(encoding="utf-8"),
@@ -20,7 +20,7 @@ def main():
     }
 
     with open("src/main/python/dependencies.modrinth.json", "r", encoding="utf-8") as f:
-        metadata["dependencies"] = json.load(f)
+        data["dependencies"] = json.load(f)
 
     with ExitStack() as stack:
         files = {}
@@ -35,8 +35,8 @@ def main():
         if not file_parts:
             raise FileNotFoundError("No Jar files found.")
 
-        metadata["file_parts"] = file_parts
-        metadata["primary_file"] = file_parts[0]
+        data["file_parts"] = file_parts
+        data["primary_file"] = file_parts[0]
 
         response = requests.post(
             "https://api.modrinth.com/v2/version",
@@ -45,7 +45,7 @@ def main():
                 "User-Agent": f"{os.environ.get("REPOSITORY")}/{os.environ.get("VERSION")}"
             },
             data={
-                "data": json.dumps(metadata)
+                "data": json.dumps(data)
             },
             files=files
         )
