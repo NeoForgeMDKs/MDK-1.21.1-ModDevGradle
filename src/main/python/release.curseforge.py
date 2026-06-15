7e9b8d0912cec47c5af33dcd8b1d427879032951
Mochi753 <179483962+mochi-753@users.noreply.github.com>
2026-06-15 19:44:21 +0900

    fix(ci): これでだめならもうしらん

--- src/main/python/release.curseforge.py
+++ src/main/python/release.curseforge.py
@@ -8,7 +8,7 @@ import requests
 def main():
     metadata = {
         "changelog": Path("CHANGELOG.md").read_text(encoding="utf-8"),
-        "changelogType": "markdown",
+        "changelogType": ["markdown"],
         "displayName": f"{os.environ.get("REPOSITORY_NAME")} {os.environ.get("VERSION")}",
         "gameVersions": [ 11779 ],  # Minecraft 1.21.1
         "gameVersionNames": ["Client", "Server", "NeoForge", "1.21.1"],
